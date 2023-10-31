7d3525d51f6dedbf5419a270c976c933bd1a03d4
Federico Colombo <fedecolombo123@gmail.com>
2023-10-31 00:03:49 -0300

    Subo archivo parcial main

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+#define BITMAP_SIZE 16
+
+int first_fit(unsigned char *bitmap, size_t bitmap_size, size_t units_needed) {
+    int consecutive_free_bits = 0;
+    int first_free_bit_index = -1;
+
+    for (int i = 0; i < bitmap_size * 8; i++) {
+        int byte = i / 8;
+        int bit = i % 8;
+
+        if (!(bitmap[byte] & (1 << bit))) {
+            if (consecutive_free_bits == 0) {
+                first_free_bit_index = i;
+            }
+            consecutive_free_bits++;
+            if (consecutive_free_bits == units_needed) {
+                for (int j = 0; j < units_needed; j++) {
+                    int byte_to_set = (first_free_bit_index + j) / 8;
+                    int bit_to_set = (first_free_bit_index + j) % 8;
+                    bitmap[byte_to_set] |= (1 << bit_to_set);
+                }
+                return first_free_bit_index;
+            }
+        } else {
+            consecutive_free_bits = 0;
+        }
+    }
+
+    return -1; // No se encontró espacio adecuado.
+}
+
+void print_bitmap(unsigned char *bitmap, size_t bitmap_size) {
+    for (int i = 0; i < bitmap_size; i++) {
+        for (int j = 7; j >= 0; j--) {
+            printf("%d", (bitmap[i] >> j) & 1);
+        }
+        if (i < bitmap_size - 1) {
+            printf(" ");
+        }
+    }
+    printf(".\n");
+}
+
+int main() {
+    unsigned char bitmap[BITMAP_SIZE] = {0};
+
+    printf("Bitmap Size: %d bytes\n", BITMAP_SIZE);
+
+    size_t units_needed;
+
+    while (1) {
+        printf("Enter the number of bits to occupy (0 to exit): ");
+        scanf("%zu", &units_needed);
+
+        if (units_needed == 0) {
+            break;
+        }
+
+        int index = first_fit(bitmap, BITMAP_SIZE, units_needed);
+
+        if (index != -1) {
+            printf("Allocated %zu units starting at index %d.\n", units_needed, index);
+            printf("Updated Bitmap: ");
+            print_bitmap(bitmap, BITMAP_SIZE);
+        } else {
+            printf("Failed to allocate %zu units.\n", units_needed);
+        }
+    }
+
+    return 0;
+}
--- my_malloc_manager.h
+++ my_malloc_manager.h
@@ -1,11 +1,14 @@
-#ifndef MY_MALLOC_MANAGER_H
-#define MY_MALLOC_MANAGER_H
-
-#include <stddef.h>
-
-#define BITMAP_SIZE 16
-
+#ifndef MY_ALLOC_MANAGER_H // prevent multiple includes
+#define MY_ALLOC_MANAGER_H 1
+// Standard includes
+#include <errno.h>  // for errno
+#include <error.h>
+#include <stddef.h> // for size_t
+#include <stdio.h>  // for I/O functions
+#include <stdlib.h> // for EXIT_FAILURE
+#include <string.h> // for string manipulation functions
 int first_fit(unsigned char *bitmap, size_t bitmap_size, size_t units_needed);
 void print_bitmap(unsigned char *bitmap, size_t bitmap_size);
 
+#define BITMAP_SIZE 16
 #endif // MY_MALLOC_MANAGER_H
