871fa2bbde0d0518e97adb19e2e8fa2d8f15bae5
Federico Colombo <fedecolombo123@gmail.com>
2023-11-01 20:53:58 -0300

    Agregamos funcion free_block

--- main.c
+++ main.c
@@ -46,29 +46,66 @@ void print_bitmap(unsigned char *bitmap, size_t bitmap_size) {
     printf(".\n");
 }
 
+void free_block(unsigned char *bitmap, size_t bitmap_size, int block_index, size_t units_to_free) {
+    if (block_index < 0 || block_index >= bitmap_size * 8) {
+        printf("Invalid block index\n");
+        return;
+    }
+
+    int byte_to_clear = block_index / 8;
+    int bit_to_clear = block_index % 8;
+
+    // Verificar si el bloque está asignado
+    if (!(bitmap[byte_to_clear] & (1 << (7 - bit_to_clear)))) {
+        printf("Block at index %d is not allocated\n", block_index);
+        return;
+    }
+
+    for (int i = 0; i < units_to_free; i++) {
+        int bit_to_clear_i = (block_index + i) % 8;
+        int byte_to_clear_i = (block_index + i) / 8;
+        bitmap[byte_to_clear_i] &= ~(1 << (7 - bit_to_clear_i));
+    }
+
+    printf("Freed %zu units starting at index %d.\n", units_to_free, block_index);
+
+    printf("Updated Bitmap: ");
+    print_bitmap(bitmap, bitmap_size);
+}
+
+
 int main() {
     unsigned char bitmap[BITMAP_SIZE] = {0};
 
     printf("Bitmap Size: %d bytes\n", BITMAP_SIZE);
 
-    size_t units_needed;
-
     while (1) {
-        printf("Enter the number of bits to occupy (0 to exit): ");
-        scanf("%zu", &units_needed);
+        printf("Enter the number of bits to occupy (0 to exit, -1 to free): ");
+        int units_needed;
+        scanf("%d", &units_needed);
 
         if (units_needed == 0) {
             break;
-        }
+        } else if (units_needed == -1) {
+            int block_index_to_free;
+            printf("Enter the block index to free: ");
+            scanf("%d", &block_index_to_free);
 
-        int index = first_fit(bitmap, BITMAP_SIZE, units_needed);
+            size_t units_to_free;
+            printf("Enter the number of bits to free: ");
+            scanf("%zu", &units_to_free);
 
-        if (index != -1) {
-            printf("Allocated %zu units starting at index %d.\n", units_needed, index);
-            printf("Updated Bitmap: ");
-            print_bitmap(bitmap, BITMAP_SIZE);
+            free_block(bitmap, BITMAP_SIZE, block_index_to_free, units_to_free);
         } else {
-            printf("Failed to allocate %zu units.\n", units_needed);
+            int index = first_fit(bitmap, BITMAP_SIZE, units_needed);
+
+            if (index != -1) {
+                printf("Allocated %d units starting at index %d.\n", units_needed, index);
+                printf("Updated Bitmap: ");
+                print_bitmap(bitmap, BITMAP_SIZE);
+            } else {
+                printf("Failed to allocate %d units.\n", units_needed);
+            }
         }
     }
 
